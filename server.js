b7ce631f16a5f1b3b25b3f3547de14e5b3dba74f
jenshin75 <49443598+jenshin75@users.noreply.github.com>
2019-08-28 18:24:26 -0500

    updated

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, {userNewUrlParser:true});
 
 // Routes
 
